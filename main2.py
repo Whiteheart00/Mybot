2f76d274d575bb1b4efe9428cf7ba7b20a25e1dc
Whiteheart00 <blackwhitereally@gmail.com>
2025-08-05 00:41:30 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -17776,4 +17776,4 @@ def receive_update():
 if __name__ == '__main__':
     bot.remove_webhook()
     bot.set_webhook(url=f"{os.environ['APP_URL']}/{TOKEN}")
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
